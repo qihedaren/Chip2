d2777f5e4584688cac642b08b4bcab17917666bc
b00819544 <b00819544@essec.edu>
2025-03-29 14:34:03 +0100

    Add files via upload

---
+++ voting-app/src/routes/bigflop/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import VotingComponent from '$lib/VotingComponent.svelte';
+    import { bigFlopVotes } from '$lib/stores.js';
+</script>
+
+<VotingComponent 
+    title="Biggest Chip Flops" 
+    items={$bigFlopVotes} 
+    store={bigFlopVotes}
+/> 
